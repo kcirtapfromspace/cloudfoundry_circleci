f92d8ef7889073dfeb10d112d49595da9d92e4f9
kcirtapfromspace <105461352+kcirtapfromspace@users.noreply.github.com>
2023-05-23 00:58:12 -0600

    got FastApi working

--- src/spacy/server.py
+++ src/spacy/server.py
@@ -7,38 +7,37 @@ import json
 from cfenv import AppEnv
 from datagen import data_generator, stop_event
 from threading import Thread, Event
-from flask import Flask, request, redirect, render_template
-from flask_wtf import FlaskForm
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from spacy_similarity import my_calc_similarity
 from db import connect_to_db
-from wtforms import SelectField, SubmitField
+from typing import List, Optional
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 env = AppEnv()
-app.config['SECRET_KEY'] = 'your-secret-key'
 port = int(os.getenv("PORT"))
+templates = Jinja2Templates(directory="templates")
+
+logging.basicConfig(filename='app.log', level=logging.INFO, 
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 # Global variable to keep track of whether we're generating data
 is_generating_data = False
 # Store the data generating thread globally
 data_gen_thread = None
 
-logging.basicConfig(filename='app.log', level=logging.INFO, 
-                    format='%(asctime)s:%(levelname)s:%(message)s')
-class UserForm(FlaskForm):
-    user_id = SelectField('User', choices=[])
-    submit = SubmitField('Find Matches')
-
-
-@app.route('/start', methods=['POST'])
+@app.post('/start')
 def start_generating_data():
     global is_generating_data, data_gen_thread
     if not is_generating_data:
         is_generating_data = True
         data_gen_thread = Thread(target=data_generator)
         data_gen_thread.start()
-    return redirect(request.referrer)
+    return {"status": "Data generation started"}
 
-@app.route('/stop', methods=['POST'])
+@app.post('/stop')
 def stop_generating_data():
     global is_generating_data
     if is_generating_data:
@@ -46,9 +45,9 @@ def stop_generating_data():
         if data_gen_thread is not None:
             stop_event.set()
             data_gen_thread.join()
-    return redirect(request.referrer)
+    return {"status": "Data generation stopped"}
 
-@app.route('/clear', methods=['POST'])
+@app.post('/clear')
 def clear_database():
     conn = connect_to_db()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -59,56 +58,48 @@ def clear_database():
     except Exception as e:
         print(f"Error occurred while clearing the database: {e}")
         conn.rollback()
-    return redirect(request.referrer)
+    return {"status": "Database cleared"}
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    form = UserForm()
+@app.get('/last_five_entries')
+async def last_five_entries():
+    conn = connect_to_db()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT * FROM public.goals ORDER BY id DESC LIMIT 5;")
+    rows = cur.fetchall()
+    return {"lastFiveEntries": [dict(row) for row in rows]}
+
+@app.get('/')
+def main(request: Request):
     conn = connect_to_db()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT DISTINCT goal_user_id FROM public.goals;")
     rows = cur.fetchall()
-    form.user_id.choices = [(row['goal_user_id'], row['goal_user_id']) for row in rows]
-    if form.validate_on_submit():
-        user_id = form.user_id.data
-        cur.execute("SELECT * FROM public.goals WHERE goal_user_id = %s;", (user_id,))
-        rows = cur.fetchall()
+    user_ids = [row['goal_user_id'] for row in rows]
+    return templates.TemplateResponse("index.html", {"request": request, "user_ids": user_ids, "is_generating_data": is_generating_data})
 
-        cur_goals_list = [row['goal'] for row in rows]
-        cur_goal_ids = [row['id'] for row in rows]
-
-        matched_goals = my_calc_similarity(user_id, cur_goals_list, cur_goal_ids)
-        return render_template('index.html', form=form, matched_goals=matched_goals, is_generating_data=is_generating_data)
-    return render_template('index.html', form=form, matched_goals=None, is_generating_data=is_generating_data)
-
-
-@app.route('/select_user', methods=['GET', 'POST'])
-def select_user():
-    form = UserForm()
+@app.get('/select_user')
+async def get_select_user(request: Request):
     conn = connect_to_db()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT DISTINCT goal_user_id FROM public.goals;")
     rows = cur.fetchall()
-    form.user_id.choices = [(row['goal_user_id'], row['goal_user_id']) for row in rows]
-    if form.validate_on_submit():
-        return redirect(url_for('compute_similarities', user_id=form.user_id.data))
-    return render_template('select_user.html', form=form)
+    user_ids = [row['goal_user_id'] for row in rows]
+    return templates.TemplateResponse('select_user.html', {"request": request, "user_ids": user_ids})
 
-@app.route('/compute_similarities/<user_id>', methods=['GET'])
-def compute_similarities(user_id):
+@app.get('/compute_similarities/{user_id}')
+def compute_similarities(user_id: str):
     conn = connect_to_db()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT * FROM public.goals WHERE goal_user_id = %s;", (user_id,))
     rows = cur.fetchall()
-
     cur_goals_list = [row['goal'] for row in rows]
     cur_goal_ids = [row['id'] for row in rows]
-
     matched_goals = my_calc_similarity(user_id, cur_goals_list, cur_goal_ids)
-    return render_template('display_matches.html', matched_goals=matched_goals)
+    return {"matched_goals": matched_goals}
+
 
 if __name__ == '__main__':
     thread = Thread(target=data_generator)
     thread.start()
-    
-    app.run(host='0.0.0.0', port=port)
+
+    uvicorn.run(app, host='0.0.0.0', port=port)
